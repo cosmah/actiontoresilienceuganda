bb5896ba512727da0ab4a9e1db09eae4d5a07cce
cosmah <cosmahke4@gmail.com>
2024-09-04 00:10:46 +0300

    added a responsive navbar

---
+++ actiontoresilienceuganda/src/components/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Home.css';
+
+const Home = (props) => {
+    return <>
+        <div>
+            
+        <main>
+        <div class="gear"></div>
+        <p class="gear-text">WELCOME TO ACTION TO RESILIENCE UGANDA</p>
+        <p class="gear-text">UNDER CONSTRUCTION, PLEASE </p>
+
+        <h3>Reach out to us at info@actiontoresilienceuganda.org
+        </h3>
+      </main>
+    
+        </div>
+    </>;
+};
+
+export default Home
---
+++ actiontoresilienceuganda/src/components/Routes/Routes.jsx
@@ -0,0 +1,60 @@
+// routes.js
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Routes.css";
+import logo from "../../assets/logo2.png";
+
+const RoutingComponent = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="nav-bar">
+      <div className="logo">
+        <Link to="/">
+          <img src={logo} alt="Logo" /> {/* Replace placeholder with logo */}
+        </Link>
+        
+      </div>
+      <ul className={`nav-links ${isOpen ? "show-menu" : ""}`}>
+        <div className="close" onClick={toggleMenu}>
+          X
+        </div>
+        <li>
+          <Link to="/">
+            <span>Home</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/services">
+            <span>Service Center</span>
+          </Link>
+        </li>
+        
+        <li>
+          <Link to="/about">
+            <span>About Us</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact">
+            <span>Contact Us</span>
+          </Link>
+        </li>
+      </ul>
+      {/* <div className="company-name">
+        <strong>Carfic AutoRepairs</strong>
+      </div> */}
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+    </nav>
+  );
+};
+
+export default RoutingComponent;
