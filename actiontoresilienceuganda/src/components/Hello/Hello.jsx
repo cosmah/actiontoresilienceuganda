e3b941792e5e99243eb2125bc599d7f945d701dc
cosmah <cosmahke4@gmail.com>
2024-09-08 16:07:21 +0300

    working on the slogan now

--- actiontoresilienceuganda/src/components/Hello/Hello.jsx
+++ actiontoresilienceuganda/src/components/Hello/Hello.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Hello.css";
 
 const Hello = (props) => {
+  const slogans = [
+    "Hello, Welcome to Action To Resilience Uganda",
+    "Empowering Communities, Enriching Lives!",
+    "Together, We Make a Difference!"
+  ];
+
+  const [currentSloganIndex, setCurrentSloganIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSloganIndex((prevIndex) => (prevIndex + 1) % slogans.length);
+    }, 10000); // Change slogan every 10 seconds
+
+    return () => clearInterval(interval); // Cleanup interval on component unmount
+  }, [slogans.length]);
+
   return (
     <div className='header'>
-      <h1>Hello, Welcome to Our NGO!</h1>
+      <div className='slogan-container'>
+        <h1 className='slogan'>{slogans[currentSloganIndex]}</h1>
+      </div>
     </div>
   );
 };
